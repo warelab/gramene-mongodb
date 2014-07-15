8fb4b9a63b4397dfe9128a41fdd046b66f407e2b
Andrew Olson <olson@cshl.edu>
2014-07-15 13:20:10 -0400

    use search_dump.pl from ajo2995/eg-web-common

--- search/indexCommands.js
+++ search/indexCommands.js
@@ -1,5 +1,4 @@
 db.genes.ensureIndex({"$**":"text"});
-db.genes.ensureIndex({taxon_id:1});
 db.genes.ensureIndex({"xrefs.GO":1});
 db.genes.ensureIndex({"xrefs.PO":1});
 db.genes.ensureIndex({"xrefs.TO":1});
@@ -8,5 +7,5 @@ db.genes.ensureIndex({"ancestors.PO":1});
 db.genes.ensureIndex({"ancestors.TO":1});
 db.genes.ensureIndex({"ancestors.taxon":1});
 db.genes.ensureIndex({biotype:1});
-db.genes.ensureIndex({taxon_id:1, seq_region:1, start:1});
+db.genes.ensureIndex({taxon_id:1, "location.seq_region":1, "location.start":1});
 db.genes.ensureIndex({domains:1});
