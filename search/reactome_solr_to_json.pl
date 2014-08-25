096522f3a3d267ceef1420fae5baa8f9da7adfdc
Andrew Olson <olson@cshl.edu>
2014-08-25 15:06:45 -0400

    more reverse-engineering

--- search/reactome_solr_to_json.pl
+++ search/reactome_solr_to_json.pl
@@ -6,8 +6,29 @@ use autodie;
 my $head = <>;
 while (<>) {
 	chomp;
-	my ($id, $title, $module, $object, $species, $taxonomy, $content) = split /\t/, $_;
+	my ($id, $title, $module, $object, $system_name, $taxonomy, $content) = split /\t/, $_;
     my ($obj_id, $pathway_id, $species_id) = $id =~ m/(\d+)-(\d+)-(\d+)$/;
-    print qq {{"object_id":$obj_id,"pathway_id":$pathway_id,"species_id":$species_id,"title":"$title","taxon_id":$taxonomy,"system_name":"$species","content":"$content"}
-};
+    my ($genus, $species, $type, $name) = $title =~ m/(\S+)\s+(\S+)\s(\S+)\s+(.+)/;
+    my $pathway;
+    if ($type eq 'pathway') {
+        $pathway = $name;
+    }
+    else {
+        ($name, $pathway) = $name =~ m/(.+?)\s*\(pathway:\s+(.+)\)/;
+    }
+    # $content =~ s/${name}\s*//;
+    $content = substr $content, length($name)+1;
+    my $class;
+    ($class,$content) = $content =~ m/(\S+)\s*(.*)/;
+    print qq{"object_id":$obj_id,"pathway_id":$pathway_id,"species_id":$species_id,"taxon_id":$taxonomy};
+    print qq{,"type":"$type","system_name":"$system_name","name":"$name","pathway":"$pathway"};
+    if ($content) {
+        if ($class eq "Reaction" or $class eq "Pathway") {
+            my $location;
+            ($location,$content) = $content =~ m/(cytosol|cytoplasm|nucleoplasm|plastid stroma|mitochondrial matrix|endoplasmic reticulum membrane|plasma membrane)\s*(\s*)/;
+            print qq{,"location":"$location"} if $location;
+        }
+    }
+    print qq{,"content":"$content"} if $content;
+    print qq{,"class":"$class"\n};
 }
