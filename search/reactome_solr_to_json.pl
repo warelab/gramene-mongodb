cdafeb947f526af52d9b3af855453c8a308240d7
Andrew Olson <olson@cshl.edu>
2014-08-25 15:12:39 -0400

    bugfix

--- search/reactome_solr_to_json.pl
+++ search/reactome_solr_to_json.pl
@@ -16,19 +16,18 @@ while (<>) {
     else {
         ($name, $pathway) = $name =~ m/(.+?)\s*\(pathway:\s+(.+)\)/;
     }
-    # $content =~ s/${name}\s*//;
     $content = substr $content, length($name)+1;
     my $class;
     ($class,$content) = $content =~ m/(\S+)\s*(.*)/;
-    print qq{{"object_id":$obj_id,"pathway_id":$pathway_id,"species_id":$species_id,"taxon_id":$taxonomy};
-    print qq{,"type":"$type","system_name":"$system_name","name":"$name","pathway":"$pathway"};
+    print "{\"object_id\":$obj_id,\"pathway_id\":$pathway_id,\"species_id\":$species_id,\"taxon_id\":$taxonomy";
+    print ",\"type\":\"$type\",\"system_name\":\"$system_name\",\"name\":\"$name\",\"pathway\":\"$pathway\"";
     if ($content) {
         if ($class eq "Reaction" or $class eq "Pathway") {
             my $location;
             ($location,$content) = $content =~ m/(cytosol|cytoplasm|nucleoplasm|plastid stroma|mitochondrial matrix|endoplasmic reticulum membrane|plasma membrane)\s*(\s*)/;
-            print qq{,"location":"$location"} if $location;
+            print ",\"location\":\"$location\"" if $location;
         }
     }
-    print qq{,"content":"$content"} if $content;
-    print qq{,"class":"$class"\n}};
+    print ",\"content\":\"$content\"" if $content;
+    print ",\"class\":\"$class\"}\n";
 }
