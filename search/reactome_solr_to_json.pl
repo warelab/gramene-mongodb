a88f9623d5cdf0219aaa7993e380fe63d0b0ff31
Andrew Olson <olson@cshl.edu>
2014-08-25 11:52:11 -0400

    convert reactome indexing data from tsv to json

---
+++ search/reactome_solr_to_json.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use autodie;
+
+my $head = <>;
+while (<>) {
+	chomp;
+	my ($id, $title, $module, $object, $species, $taxonomy, $content) = split /\t/, $_;
+    my ($obj_id, $pathway_id, $species_id) = $id =~ m/(\d+)-(\d+)-(\d+)$/;
+    print qq {{"object_id":$obj_id,"pathway_id":$pathway_id,"species_id":$species_id,"title":"$title","species":"$species","taxon_id":$taxonomy,"system_name":"$species","content":"$content"}
+};
+}
