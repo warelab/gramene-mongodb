b80c5313bab72e0f56a3bfb0832984471ddc649e
Andrew Olson <olson@cshl.edu>
2014-08-25 12:25:09 -0400

    rm duplicate field

--- search/reactome_solr_to_json.pl
+++ search/reactome_solr_to_json.pl
@@ -8,6 +8,6 @@ while (<>) {
 	chomp;
 	my ($id, $title, $module, $object, $species, $taxonomy, $content) = split /\t/, $_;
     my ($obj_id, $pathway_id, $species_id) = $id =~ m/(\d+)-(\d+)-(\d+)$/;
-    print qq {{"object_id":$obj_id,"pathway_id":$pathway_id,"species_id":$species_id,"title":"$title","species":"$species","taxon_id":$taxonomy,"system_name":"$species","content":"$content"}
+    print qq {{"object_id":$obj_id,"pathway_id":$pathway_id,"species_id":$species_id,"title":"$title","taxon_id":$taxonomy,"system_name":"$species","content":"$content"}
 };
 }
