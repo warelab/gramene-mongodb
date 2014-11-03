b03309b1a1e4bb8e97a7d6454f69468cf3225b75
Andrew Olson <olson@cshl.edu>
2014-11-03 16:45:53 -0500

    parser for interpro domains

--- ontologies/parseInterpro.js
+++ ontologies/parseInterpro.js
@@ -1,39 +1,64 @@
-var filename = process.argv[2];
+// download interpro and uncompress before running this script
+// 'ftp://ftp.ebi.ac.uk/pub/databases/interpro/interpro.xml.gz';
+// 'ftp://ftp.ebi.ac.uk/pub/databases/interpro/ParentChildTreeFile.txt'
+
+
+var parentChildFile = process.argv[2];
+var xmlFile = process.argv[3];
 
 var fs        = require('fs')
   , XmlStream = require('xml-stream');
 
-// Create a file stream and pass it to XmlStream
-var stream = fs.createReadStream(filename);
-var xml = new XmlStream(stream);
+// read the parentChildFile into memory
+var ancestors = {}; // key is child, value is list of ancestors
+var currentNodes = [];
+var currentDepth = 0;
+require('readline').createInterface({
+  input: fs.createReadStream(parentChildFile),
+  terminal: false
+}).on('line', function(line) {
+  var splittedLine = line.split('--');
+  var depth = splittedLine.length - 1;
+  var IPRwithNames = splittedLine[depth];
+  var splittedIPR = IPRwithNames.split('::');
+  var IPR = parseInt(splittedIPR[0].match(/\d+/));
+  currentNodes[depth] = IPR;
+  ancestors[IPR] = [];
+  for (var i = depth; i>=0; i--) {
+    ancestors[IPR].push(currentNodes[i])
+  }
+}).on('close', function() {
+  // Create a file stream and pass it to XmlStream
+  var stream = fs.createReadStream(xmlFile);
+  var xml = new XmlStream(stream);
 
-// xml.preserve('interpro', true);
-xml.collect('db_xref');
-xml.on('endElement: interpro', function(item) {
-  // console.log(item);
-  var obj = {};
-  obj._id = parseInt(item.$.id.match(/\d+/));
-  obj.id = item.$.id;
-  obj.short_name = item.$.short_name;
-  obj.type = item.$.type;
-  obj.name = item.name;
-  if (item.hasOwnProperty('abstract') && item.abstract.hasOwnProperty('p')) obj.abstract = item.abstract.p.$text;
-  if (item.hasOwnProperty('member_list') && item.member_list.hasOwnProperty('db_xref')) {
-    item.member_list.db_xref.forEach(function(xref) {
-      obj[xref.$.db] = xref.$.dbkey;
-    });
-  }
-  if (item.hasOwnProperty('external_doc_list') && item.external_doc_list.hasOwnProperty('db_xref')) {
-    item.external_doc_list.db_xref.forEach(function(xref) {
-      obj[xref.$.db] = xref.$.dbkey;
-    });
-  }
-  if (item.hasOwnProperty('structure_db_links') && item.structure_db_links.hasOwnProperty('db_xref')) {
-    item.structure_db_links.db_xref.forEach(function(xref) {
-      if (! obj.hasOwnProperty(xref.$.db)) obj[xref.$.db] = [];
-      obj[xref.$.db].push(xref.$.dbkey);
-    })
-  }
-  console.log(JSON.stringify(obj));
-  // xml.pause();
+  xml.collect('db_xref');
+  xml.on('endElement: interpro', function(item) {
+    // console.log(item);
+    var obj = {};
+    obj._id = parseInt(item.$.id.match(/\d+/));
+    obj.ancestors = (ancestors[obj._id]) ? ancestors[obj._id] : [obj._id];
+    obj.id = item.$.id;
+    obj.short_name = item.$.short_name;
+    obj.type = item.$.type;
+    obj.name = item.name;
+    if (item.hasOwnProperty('abstract') && item.abstract.hasOwnProperty('p')) obj.abstract = item.abstract.p.$text;
+    if (item.hasOwnProperty('member_list') && item.member_list.hasOwnProperty('db_xref')) {
+      item.member_list.db_xref.forEach(function(xref) {
+        obj[xref.$.db] = xref.$.dbkey;
+      });
+    }
+    if (item.hasOwnProperty('external_doc_list') && item.external_doc_list.hasOwnProperty('db_xref')) {
+      item.external_doc_list.db_xref.forEach(function(xref) {
+        obj[xref.$.db] = xref.$.dbkey;
+      });
+    }
+    if (item.hasOwnProperty('structure_db_links') && item.structure_db_links.hasOwnProperty('db_xref')) {
+      item.structure_db_links.db_xref.forEach(function(xref) {
+        if (! obj.hasOwnProperty(xref.$.db)) obj[xref.$.db] = [];
+        obj[xref.$.db].push(xref.$.dbkey);
+      })
+    }
+    console.log(JSON.stringify(obj));
+  });
 });
