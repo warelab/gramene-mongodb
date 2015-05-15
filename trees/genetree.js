c8475b571f77ace77233456fd57e92e5f076a1b3
mycrobe <jmulvane@cshl.edu>
2015-05-15 15:29:58 -0400

    v minor cleanup

--- trees/genetree.js
+++ trees/genetree.js
@@ -24,24 +24,24 @@ var query = "select " +
   "g.assembly, a.node_type, a.bootstrap, a.duplication_confidence_score, " +
   "sn.taxon_id as node_taxon_id, sn.node_name as node_taxon " +
 
-"from gene_tree_root r " +
-"inner join gene_tree_node n on n.root_id = r.root_id " +
+  "from gene_tree_root r " +
+  "inner join gene_tree_node n on n.root_id = r.root_id " +
 
-"left join seq_member s on s.seq_member_id = n.seq_member_id " +
-"left join genome_db g on g.genome_db_id = s.genome_db_id " +
+  "left join seq_member s on s.seq_member_id = n.seq_member_id " +
+  "left join genome_db g on g.genome_db_id = s.genome_db_id " +
 
-"left join gene_tree_node_attr a on a.node_id = n.node_id " +
-"left join species_tree_node sn on sn.node_id = a.species_tree_node_id " +
+  "left join gene_tree_node_attr a on a.node_id = n.node_id " +
+  "left join species_tree_node sn on sn.node_id = a.species_tree_node_id " +
 
-"where r.tree_type = 'tree' and clusterset_id = 'default' " +
+  "where r.tree_type = 'tree' and clusterset_id = 'default' " +
 
-"order by r.root_id";
+  "order by r.root_id";
 
-var tidyRow = through2.obj(function(row, encoding, done) {
+var tidyRow = through2.obj(function (row, encoding, done) {
   // FlatToNested does not like it if parent is defined on root.
   // (This happens with Compara because tree roots are considered
   // to be nodes in a higher order tree)
-  if(row.root_id === row.node_id) {
+  if (row.root_id === row.node_id) {
     // We COULD store the reference to the supertree here, but
     // what is the use case?
 
@@ -51,15 +51,15 @@ var tidyRow = through2.obj(function(row, encoding, done) {
   }
 
   // remove null properties
-  this.push( _.omit(row, _.isNull));
+  this.push(_.omit(row, _.isNull));
   done();
 });
 
-var groupRowsByTree = function() {
+var groupRowsByTree = function () {
   var growingTree;
 
-  var transform = function(row, enc, done) {
-    if(growingTree && growingTree.treeId === row.tree_stable_id) {
+  var transform = function (row, enc, done) {
+    if (growingTree && growingTree.treeId === row.tree_stable_id) {
       growingTree.nodes.push(row);
     }
     else {
@@ -69,12 +69,12 @@ var groupRowsByTree = function() {
         nodes: [row]
       };
 
-      if(doneTree) {
+      if (doneTree) {
         this.push(doneTree);
       }
     }
 
-    if(row.node_id !== row.root_id) {
+    if (row.node_id !== row.root_id) {
       delete row.root_id;
       delete row.tree_stable_id;
     }
@@ -82,7 +82,7 @@ var groupRowsByTree = function() {
     done();
   };
 
-  var flush = function(done) {
+  var flush = function (done) {
     //console.log('group rows by tree is done');
     this.push(growingTree);
     done();
@@ -91,24 +91,24 @@ var groupRowsByTree = function() {
   return through2.obj(transform, flush);
 };
 
-var makeNestedTree = through2.obj(function(tree, enc, done) {
+var makeNestedTree = through2.obj(function (tree, enc, done) {
   tree.nested = new FlatToNested({id: 'node_id', parent: 'parent_id', children: 'children'}).convert(tree.nodes);
   tree.nested._id = tree.nested.tree_stable_id;
   this.push(tree);
   done();
 });
 
-var loadIntoTreeModelAndDoAQuickSanityCheck = through2.obj(function(tree, enc, done) {
+var loadIntoTreeModelAndDoAQuickSanityCheck = through2.obj(function (tree, enc, done) {
   var isSupertree = tree.nested.tree_stable_id.indexOf('Supertree') === 0;
   tree.treeModel = treeModel.parse(tree.nested);
-  tree.treeModel.all(function(node) {
-    if(node.children && node.children.length) {
-      if(node.model.stable_id) {
+  tree.treeModel.all(function (node) {
+    if (node.children && node.children.length) {
+      if (node.model.stable_id) {
         console.log('Found branch node associated with a gene', node.model.stable_id, node.model.node_id);
       }
     }
     else {
-      if(node.model.node_type && !isSupertree) {
+      if (node.model.node_type && !isSupertree) {
         console.log('Found leaf node with property meant for branch node', node.model.node_type, node.model.node_id);
       }
     }
@@ -118,21 +118,21 @@ var loadIntoTreeModelAndDoAQuickSanityCheck = through2.obj(function(tree, enc, d
   done();
 });
 
-var counter = function() {
+var counter = function () {
   var treeCount = 0;
   var rowCount = 0;
 
-  var transform = function(tree, enc, done) {
+  var transform = function (tree, enc, done) {
     ++treeCount;
     rowCount += tree.nodes.length;
-    if(treeCount % 1000 == 0) {
+    if (treeCount % 1000 == 0) {
       console.log(treeCount + ' trees with ' + rowCount + ' nodes so far');
     }
     this.push(tree);
     done();
   };
 
-  var flush = function(done) {
+  var flush = function (done) {
     console.log(treeCount + ' trees with ' + rowCount + ' nodes in total');
     done();
   };
@@ -140,23 +140,30 @@ var counter = function() {
   return through2.obj(transform, flush);
 };
 
-var serialize = through2.obj(function(r, enc, done) {
+var serialize = through2.obj(function (r, enc, done) {
   this.push(JSON.stringify(r) + "\n");
   done();
-}, function(done) { console.log('serializer is done'); done()});
+}, function (done) {
+  console.log('serializer is done');
+  done()
+});
 
 var fileWriter = fs.createWriteStream('./inserts.jsonl');
 
-MongoClient.connect('mongodb://127.0.0.1:27017/search45', function(err, mongoDb) {
-
+MongoClient.connect('mongodb://127.0.0.1:27017/search45', function (err, mongoDb) {
   var mongoCollection = mongoDb.collection('genetree');
 
-  var upsertTreeIntoMongo = through2.obj(function(tree, enc, done) {
+  var upsertTreeIntoMongo = through2.obj(function (tree, enc, done) {
     var throughThis = this;
-    mongoCollection.update({_id: tree.nested._id}, tree.nested, {upsert: true}, function(err, count, status) {
-      throughThis.push({err: err, status: status, _id: tree.nested._id});
-      done();
-    });
+    mongoCollection.update(
+      {_id: tree.nested._id},
+      tree.nested,
+      {upsert: true},
+      function (err, count, status) {
+        throughThis.push({err: err, status: status, _id: tree.nested._id});
+        done();
+      }
+    );
   });
 
   var stream = comparaMysqlDb.query(query)
@@ -170,7 +177,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017/search45', function(err, mongoDb)
     .pipe(serialize)
     .pipe(fileWriter);
 
-  fileWriter.on('finish', function() {
+  fileWriter.on('finish', function () {
     console.log('We are done here.');
     stream.end();
 
