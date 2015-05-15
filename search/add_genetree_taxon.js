5df26ec23d630d01a6b9ff916ace77b1e28e0aa7
mycrobe <jmulvane@cshl.edu>
2015-05-15 15:25:22 -0400

    add epl_gene_tree_root_taxon_id to gene docs

---
+++ search/add_genetree_taxon.js
@@ -0,0 +1,34 @@
+var mongoURL = 'mongodb://127.0.0.1:27017/search45';
+
+var MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+var _ = require('lodash');
+
+var filename = process.argv[2];
+
+function modifyGeneDocs(genetreeLUT) {
+  require('readline').createInterface({
+    input: fs.createReadStream(filename),
+    terminal: false
+  }).on('line', function (line) { // one JSON object per line
+    var obj = JSON.parse(line);
+    if(obj.epl_gene_tree) {
+      obj.epl_gene_tree_root_taxon_id = genetreeLUT[obj.epl_gene_tree];
+    }
+    console.log(JSON.stringify(obj));
+  }).on('close', function () {
+    // close the database connection, but give the currently running commands
+    setTimeout(function () {
+      db.close();
+    }, 5000);
+  });
+}
+
+// connect to the ontologies database
+MongoClient.connect(mongoURL, function(err, db) {
+  if (err) throw err;
+  db.collection('genetree').find({}, {node_taxon_id: 1}).toArray(function(err, docs) {
+    if (err) throw err;
+    modifyGeneDocs(_.indexBy(docs, '_id'));
+  });
+});
