75b43e41942e5ba7c0171ef7b5a2f7a535855ddc
Andrew Olson <olson@cshl.edu>
2015-06-30 16:53:39 -0400

    updates to genetrees in solr

--- search/solr/mongo2solr.js
+++ search/solr/mongo2solr.js
@@ -61,8 +61,8 @@ require('readline').createInterface(
     solr.canonical_translation = mongo.canonical_translation.name;
     solr.canonical_translation_length = mongo.canonical_translation['length'];
   }
-  if (mongo.hasOwnProperty('epl_gene_tree_root_taxon_id')) {
-    solr.epl_gene_tree_root_taxon_id = mongo.epl_gene_tree_root_taxon_id;
+  if (mongo.hasOwnProperty('grm_gene_tree_root_taxon_id')) {
+    solr.grm_gene_tree_root_taxon_id = mongo.grm_gene_tree_root_taxon_id;
   }
   
   for (var field in mongo) {
@@ -80,6 +80,7 @@ require('readline').createInterface(
   
   if (mongo.hasOwnProperty("eg_gene_tree"))  solr.eg_gene_tree  = mongo.eg_gene_tree;
   if (mongo.hasOwnProperty("epl_gene_tree")) solr.epl_gene_tree = mongo.epl_gene_tree;
+  if (mongo.hasOwnProperty("grm_gene_tree")) solr.grm_gene_tree = mongo.grm_gene_tree;
   if (mongo.hasOwnProperty("pathways"))      solr.pathways      = mongo.pathways;
   if (mongo.hasOwnProperty("reactions"))     solr.reactions     = mongo.reactions;
 
