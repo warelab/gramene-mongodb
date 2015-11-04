6f294264656d6ba3ba629234f2a07b13b78996f8
mycrobe <jmulvane@cshl.edu>
2015-11-04 16:32:06 -0500

    rename collection from 'genetree' to 'genetrees' to be consistent with 'genes', etc.

--- search/indexCommands.js
+++ search/indexCommands.js
@@ -3,7 +3,7 @@ db.genes.ensureIndex({"$**":"text"});
 db.genetree.ensureIndex({"$**":"text"});
 db.GO.ensureIndex({"$**":"text"});
 db.PO.ensureIndex({"$**":"text"});
-db.NCBITaxon.ensureIndex({"$**":"text"});
+db.taxonomy.ensureIndex({"$**":"text"});
 db.domains.ensureIndex({"$**":"text"});
 db.pathways.ensureIndex({"$**":"text"});
 db.maps.ensureIndex({"$**":"text"});
