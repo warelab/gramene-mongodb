0d956297ce048a673a42d425f66b5933ba44bcf9
Andrew Olson <olson@cshl.edu>
2016-11-04 10:57:32 -0400

    deal with updated v3v4 mappings

--- search/fix_maize_v4.js
+++ search/fix_maize_v4.js
@@ -6,7 +6,7 @@ var _ = require('lodash');
 function getMapping() {
   var deferred = Q.defer();
   var lut = {};
-  var filename = 'v3v4.gene_ID_history_with_names.txt';
+  var filename = 'geneIDhistory.v3TOv4.txt.full_info.oct2016.name.description';
   console.error('fixMaizeV4 getMapping');
   require('readline').createInterface({
     input: require('fs').createReadStream(filename),
@@ -16,23 +16,21 @@ function getMapping() {
     var fields = line.split("\t");
     /*
       fields are
-    v3 id
-    float
-    v4 id
-    float
-    v3 name
-    v3 description
+  0  v3 id
+  1  v4 id
+  2  a
+  3  b
+  4  c
+  5  v3 name
+  6  v3 description
     */
-    if (fields.length === 5) {
-      fields.push(''); // because the description might be missing
+    if (lut.hasOwnProperty(fields[1])) {
+      lut[fields[1]].v3.push(fields[0]);
+      lut[fields[1]].names.push(fields[5]);
+      lut[fields[1]].descriptions.push(fields[6]);
     }
-    if (fields.length === 6) {
-      if (lut.hasOwnProperty(fields[2])) {
-        lut[fields[2]].synonyms.push(fields[0]);
-      }
-      else {
-        lut[fields[2]] = {id:fields[0], synonyms:[fields[0]], name:fields[4], description:fields[5]};
-      }
+    else {
+      lut[fields[1]] = {v3:[fields[0]], names:[fields[5]], descriptions:[fields[6]]};
     }
   })
   .on('close', function() {
@@ -55,13 +53,34 @@ module.exports = function() {
   
     lutPromise.then(function(lut) {
       if (lut[gene._id]) {
-        var v3 = lut[gene._id];
-        gene.synonyms = v3.synonyms;
-        if (v3.name != v3.id) {
-          gene.name = v3.name;
+        // merge with gene.synonyms
+        if (!gene.hasOwnProperty('synonyms')) {
+          gene.synonyms = [];
         }
-        if (v3.description && !v3.description.match(/Uncharacterized protein/)) {
-          gene.description = v3.description;
+        lut[gene._id].v3.forEach({id} => {
+          if (id != '-') {
+            gene.synonyms.push(id);
+          }
+        });
+        var v3names = [];
+        lut[gene._id].names.forEach({v3name} => {
+          if (v3name != '-') {
+            gene.synonyms.push(v3name);
+            v3names.push(v3name);
+          }
+        });
+        if (gene.name == gene._id && v3names.length == 1) {
+          gene.name = v3names[0];
+        }
+        gene.synonyms = _.uniq(gene.synonyms);
+        var v3descriptions = [];
+        lut[gene._id].descriptions.forEach({v3description} => {
+          if (v3description != '-' && !v3description.match(/Uncharacterized protein/)) {
+            v3descriptions.push(v3description);
+          }
+        });
+        if (!gene.description && v3descriptions.length == 1) {
+          gene.description = v3descriptions[0];
         }
       }
       that.push(gene);
