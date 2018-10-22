d1520dca02d52defb4c51c64c0d1e3dd5235a93b
Andrew Olson <olson@cshl.edu>
2018-10-22 12:47:55 -0400

    use dev server for PR

--- reactome/get_pathways.js
+++ reactome/get_pathways.js
@@ -21,7 +21,7 @@ var collections = require('gramene-mongodb-config');
 
 var genesToReactionsFile = argv.gtr || 'Ensembl2PlantReactomeReactions.txt';
 var geneProductMappingFile = argv.gtp || 'gene_ids_by_pathway_and_species.tab';
-var api = argv.api || 'http://plantreactome.gramene.org/ContentService';
+var api = argv.api || 'http://plantreactomedev.oicr.on.ca/ContentService';
 
 var docs = {};
 var taxonLUT = {}; // key is speciesCode, value is taxon id
--- reactome/get_species_prefixes.js
+++ reactome/get_species_prefixes.js
@@ -13,19 +13,37 @@ function getReactomePrefix(tax_id) {
     return top[0].stId.replace(/R-([A-Z]*)-\d+/,'$1');
   }
 }
-
-collections.maps.mongoCollection().then(function(mapsCollection) {
-  mapsCollection.find({},{taxon_id:1}).toArray(function (err, docs) {
-    collections.closeMongoDatabase();
-    var taxonomy = {};
-    docs.forEach(function(doc) {
-      var prefix = getReactomePrefix(doc.taxon_id);
-      if (prefix) {
-        taxonomy[doc.taxon_id] = {
-          reactomePrefix: prefix
-        };
-      }
-    });
-    console.log(JSON.stringify(taxonomy));
+var url = 'http://plantreactomedev.oicr.on.ca/ContentService/data/species/all';
+var res = request('GET', url);
+if (res.statusCode == 200) {
+  var PRspecies = JSON.parse(res.getBody());
+  var lut = {};
+  PRspecies.forEach(function(s) {
+    lut[s.taxId] = s.abbreviation;
+    lut[s.displayName] = s.abbreviation;
   });
-});
+  collections.maps.mongoCollection().then(function(mapsCollection) {
+    mapsCollection.find({},{taxon_id:1,display_name:1}).toArray(function (err, docs) {
+      var taxonomy = {};
+      collections.closeMongoDatabase();
+      docs.forEach(function(doc) {
+        var prefix;
+        if (lut[doc.taxon_id]) {
+          prefix = lut[doc.taxon_id];
+        } else if(lut[doc.display_name]) {
+          prefix = lut[doc.display_name];
+        }
+        if (prefix) {
+          taxonomy[doc.taxon_id] = {
+            reactomePrefix: prefix
+          };
+        }
+        else {
+          console.error("no reactome prefix found for map ", $doc);
+        }
+      });
+      console.log(JSON.stringify(taxonomy));
+    });
+  });
+}
+
